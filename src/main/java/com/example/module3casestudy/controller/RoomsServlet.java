4ee9a9e662403b3db5027c9b3962600d5f44f63d
SonSon-CG <son.mup@gmail.com>
2025-02-11 14:25:04 +0700

    dang-lam

--- src/main/java/com/example/module3casestudy/controller/RoomsServlet.java
+++ src/main/java/com/example/module3casestudy/controller/RoomsServlet.java
@@ -1,4 +1,10 @@
 package com.example.module3casestudy.controller;
 
-public class RoomsServlet {
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "rooms", value ="/rooms")
+public class RoomsServlet extends HttpServlet {
+
 }
--- src/main/java/com/example/module3casestudy/service/IRoomsService.java
+++ src/main/java/com/example/module3casestudy/service/IRoomsService.java
@@ -1,4 +1,16 @@
 package com.example.module3casestudy.service;
 
+import com.example.module3casestudy.model.Rooms;
+
+import java.util.List;
+
 public interface IRoomsService {
+
+    List<Rooms> findAll();
+
+    void create(Rooms rooms);
+
+    void save(Rooms rooms);
+
+
 }
